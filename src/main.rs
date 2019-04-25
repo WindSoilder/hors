5822e2397a899d56dd3e9a865dab62c7b209f12a
WindSoilder <WindSoilder@outlook.com>
2019-04-25 13:02:30 +0800

    finish argument parser, and update dependency

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+extern crate clap;
+use clap::{App, Arg, ArgMatches};
+
+fn parser_matches<'a>() -> ArgMatches<'a> {
+    let parser = App::new("howrs")
+        .author("WindSoilder, WindSoilder@outlook.com")
+        .version("0.1.0")
+        .arg(
+            Arg::with_name("all")
+                .long("all")
+                .short("a")
+                .help("display the full text of the answer."),
+        )
+        .arg(
+            Arg::with_name("link")
+                .long("link")
+                .short("l")
+                .help("display only the answer link."),
+        )
+        .arg(
+            Arg::with_name("color")
+                .long("color")
+                .short("c")
+                .help("enable colorized output"),
+        )
+        .arg(
+            Arg::with_name("number_answers")
+                .long("number_answers")
+                .short("n")
+                .takes_value(true)
+                .help("number of answers to return"),
+        )
+        .arg(
+            Arg::with_name("version")
+                .long("version")
+                .short("v")
+                .help("displays the current version of howdoi"),
+        )
+        .arg(
+            Arg::with_name("QUERY")
+        );
+    return parser.get_matches();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let matches = parser_matches();
 }
