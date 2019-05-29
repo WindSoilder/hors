a3fa0b2d4500c858b112925586e6bce6cdefa659
WindSoilder <WindSoilder@outlook.com>
2019-05-29 09:36:31 +0800

    finish testing for get_answers.

--- tests/test_answer.rs
+++ tests/test_answer.rs
@@ -1,6 +1,75 @@
-use hors::answer::get_answers;
+use hors::answer::{get_answers, AnswerRecordsCache, SPLITTER};
+use hors::config::{Config, OutputOption, SearchEngine};
 
 #[test]
-fn test_get_answer() {
-
+fn test_get_answers_with_links_only() {
+    let links: Vec<String> = vec![String::from(
+        "https://stackoverflow.com/questions/7771011/parse-json-in-python",
+    )];
+    let conf: Config = Config::new(OutputOption::Links, 10, false);
+    let answers: String = get_answers(&links, conf)
+        .expect("Get answer through stackoverflow should success")
+        .split(SPLITTER)
+        .collect();
+    // each answer itself only contains Title: xxx\nhttps://stackoverflow.com/xx
+    assert_eq!(
+        answers,
+        "Title - parse json in python
+https://stackoverflow.com/questions/7771011/parse-json-in-python"
+    );
 }
+
+#[test]
+fn test_get_answers_with_detailed_option() {
+    let links: Vec<String> = vec![String::from(
+        "https://stackoverflow.com/questions/7771011/parse-json-in-python",
+    )];
+    let conf: Config = Config::new(OutputOption::All, 10, false);
+    let answers: String = get_answers(&links, conf)
+        .expect("Get answer through stackoverflow should success")
+        .split(SPLITTER)
+        .collect();
+
+    assert_eq!(
+        answers.trim(),
+        "- Answer from https://stackoverflow.com/questions/7771011/parse-json-in-python
+
+Very simple:
+
+import json
+j = json.loads('{\"one\" : \"1\", \"two\" : \"2\", \"three\" : \"3\"}')
+print j['two']"
+    )
+}
+
+#[test]
+fn test_get_answers_with_instruction() {
+    let links: Vec<String> = vec![String::from(
+        "https://stackoverflow.com/questions/7771011/parse-json-in-python",
+    )];
+    let conf: Config = Config::new(OutputOption::OnlyCode, 10, false);
+    let answers: String = get_answers(&links, conf)
+        .expect("Get answer through stackoverflow should success")
+        .split(SPLITTER)
+        .collect();
+    assert_eq!(
+        answers,
+        "- Answer from https://stackoverflow.com/questions/7771011/parse-json-in-python
+import json
+j = json.loads('{\"one\" : \"1\", \"two\" : \"2\", \"three\" : \"3\"}')
+print j['two']
+"
+    )
+}
+
+//
+// Testing for AnswerRecordsCache.
+//
+#[test]
+fn test_answer_records_cache_load() {}
+
+#[test]
+fn test_answer_records_cache_save() {}
+
+#[test]
+fn test_answer_records_cache_clear() {}
