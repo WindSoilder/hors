7642ae33d1d8ea1f7bc53565b250059c7178c5ee
WindSoilder <WindSoilder@outlook.com>
2019-08-29 17:35:53 +0800

    add integration test for duckduckgo search engine.

--- tests/test_engine.rs
+++ tests/test_engine.rs
@@ -41,3 +41,21 @@ fn test_search_links_with_google_search_engine() {
         assert!(link.contains("stackoverflow.com"));
     }
 }
+
+#[test]
+fn test_search_links_with_duckduckgo_search_engine() {
+    let search_engine: SearchEngine = SearchEngine::from_str("duckduckgo").unwrap();
+    let client: Client = ClientBuilder::new().cookie_store(true).build().unwrap();
+    let target_links: Vec<String> = search_links(
+        &String::from("how to parse json in rust"),
+        search_engine,
+        &client,
+    )
+    .unwrap();
+    // for search results, what we can do is checking if
+    // target_links' host is stackoverflow.com
+    assert_ne!(target_links.len(), 0);
+    for link in target_links {
+        assert!(link.contains("stackoverflow.com"));
+    }
+}
