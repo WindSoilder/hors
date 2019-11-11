ce00de597df728cf2cabb56f78dd731e4c5ae85f
WindSoilder <WindSoilder@outlook.com>
2019-11-11 17:48:41 +0800

    add documentation for answer mod

--- src/answer/mod.rs
+++ src/answer/mod.rs
@@ -1,3 +1,7 @@
+//! Extract answers from given stackoverflow links.
+//!
+//! For more usage example, please check the root of lib documentation.
+
 mod precious;
 mod records;
 pub use precious::{get_answers, SPLITTER};
