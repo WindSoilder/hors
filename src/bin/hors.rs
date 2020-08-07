376b08ea735bdbc2e3bba2674860947f076a1898
WindSoilder <WindSoilder@outlook.com>
2020-08-07 08:55:38 +0800

    use clap new feature to make less code

--- src/bin/hors.rs
+++ src/bin/hors.rs
@@ -1,72 +1,51 @@
 #[macro_use]
 extern crate log;
 
-use clap::{App, Arg, ArgMatches};
+use clap::{self, Clap};
 use hors::{self, Config, OutputOption, Result, SearchEngine};
 use reqwest::{Client, ClientBuilder};
 
 use std::process;
 use std::str::FromStr;
 
-fn parser_matches<'a>() -> ArgMatches<'a> {
-    let parser = App::new(env!("CARGO_PKG_NAME"))
-        .author(env!("CARGO_PKG_AUTHORS"))
-        .version(env!("CARGO_PKG_VERSION"))
-        .about(env!("CARGO_PKG_DESCRIPTION"))
-        .arg(
-            Arg::with_name("all")
-                .long("all")
-                .short("a")
-                .help("display the full text of the answer."),
-        )
-        .arg(
-            Arg::with_name("link")
-                .long("link")
-                .short("l")
-                .help("display only the answer link."),
-        )
-        .arg(
-            Arg::with_name("raw")
-                .long("raw")
-                .short("r")
-                .help("make raw output (not colorized).")
-        )
-        .arg(
-            Arg::with_name("number_answers")
-                .long("number_answers")
-                .short("n")
-                .takes_value(true)
-                .default_value("1")
-                .help("number of answers to return."),
-        )
-        .arg(
-            Arg::with_name("engine")
-                .long("engine")
-                .short("e")
-                .takes_value(true)
-                .default_value("duckduckgo")
-                .help("select middle search engine, currently support `bing`, `google` and `duckduckgo`."),
-        )
-        .arg(
-            Arg::with_name("disable_proxy")
-                .long("disable_proxy")
-                .help("Disable system proxy."),
-        )
-        .arg(Arg::with_name("QUERY").required(true));
-    parser.get_matches()
+#[derive(Clap)]
+#[clap(version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"), about = env!("CARGO_PKG_DESCRIPTION"))]
+struct Opts {
+    #[clap(short, long, about("display the full text of answer."))]
+    all: bool,
+    #[clap(short, long, about("display only the answer link."))]
+    link: bool,
+    #[clap(short, long, about("make raw output (not colorized)."))]
+    raw: bool,
+    #[clap(short, long, default_value = "1", about("number of answers to return."))]
+    number_answers: u8,
+    #[clap(
+        short,
+        long,
+        default_value = "duckduckgo",
+        about("select middle search engine, currently support `bing`, `google` and `duckduckgo`.")
+    )]
+    engine: String,
+    #[clap(short, long, about("Disable system proxy."))]
+    disable_proxy: bool,
+    query: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let matches: ArgMatches = parser_matches();
+    let opts: Opts = Opts::parse();
 
-    let search_engine =
-        SearchEngine::from_str(matches.value_of("engine").unwrap_or_default()).unwrap();
+    let search_engine = SearchEngine::from_str(&opts.engine);
+    if let Err(e) = &search_engine {
+        eprintln!("Error occured while parsing `--engine` value: {}", e);
+    }
+    let search_engine = search_engine.unwrap();
     debug!("Search under the {:?}", search_engine);
 
     // Initialize reqwest::Client instance.
     let mut client_builder: ClientBuilder = reqwest::ClientBuilder::new().cookie_store(true);
-    if matches.is_present("disable_proxy") {
+    if opts.disable_proxy {
+        println!("disable proxy");
         client_builder = client_builder.no_proxy();
     }
     let client: Client = client_builder.build().unwrap_or_else(|err| {
@@ -75,14 +54,14 @@ async fn main() -> Result<()> {
     });
 
     let target_links: Vec<String> =
-        hors::search_links_with_client(matches.value_of("QUERY").unwrap(), search_engine, &client)
+        hors::search_links_with_client(&opts.query, search_engine, &client)
             .await
             .unwrap_or_else(|err| {
                 eprintln!("Search for target link failed: {}", err);
                 process::exit(1);
             });
 
-    let conf: Config = init_config(&matches);
+    let conf: Config = init_config(&opts);
     debug!("User config: {:?}", conf);
     let answers: String = hors::get_answers_with_client(&target_links, conf, &client)
         .await
@@ -96,22 +75,14 @@ async fn main() -> Result<()> {
 }
 
 /// initialize config from user input arguments.
-fn init_config(matches: &ArgMatches) -> Config {
-    let output_option = if matches.is_present("link") {
+fn init_config(opts: &Opts) -> Config {
+    let output_option = if opts.link {
         OutputOption::Links
-    } else if matches.is_present("all") {
+    } else if opts.all {
         OutputOption::All
     } else {
         OutputOption::OnlyCode
     };
 
-    Config::new(
-        output_option,
-        matches
-            .value_of("number_answers")
-            .unwrap_or_default()
-            .parse::<u8>()
-            .unwrap(),
-        !matches.is_present("raw"),
-    )
+    Config::new(output_option, opts.number_answers, !opts.raw)
 }
