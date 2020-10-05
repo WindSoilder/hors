ba7b77e67451894094731df7913e4a2078da94e1
WindSoilder <WindSoilder@outlook.com>
2020-10-05 19:29:08 +0800

    fix new style

--- src/engine/google.rs
+++ src/engine/google.rs
@@ -1,6 +1,7 @@
 use super::Engine;
 use select::document::Document;
 use select::predicate::{Class, Name, Predicate};
+use url::Url;
 
 pub struct Google;
 
@@ -21,20 +22,30 @@ impl Engine for Google {
 
     fn extract_links(&self, page: &str) -> Option<Vec<String>> {
         let doc: Document = Document::from(page);
-        // use child rather than decendent, because in google search engine
-        // a node's structure is like this:
-        // <div class="r">
-        //   <a href="test_link"></a>
-        //   <span><a href="not we need"></a></span>
-        // </div>
 
-        // make decision on class which contains target element.
+        // new style target elements, which have rc class.
         let target_elements = doc.find(Class("rc").descendant(Name("a")));
         let mut links: Vec<String> = target_elements
             .filter_map(|node| node.attr("href"))
-            .map(String::from)
+            .filter_map(|link| {
+                if let Ok(url) = Url::parse(link) {
+                    if let Some(host) = url.host_str() {
+                        if host.contains("stackoverflow.com") {
+                            return Some(String::from(link));
+                        }
+                    }
+                }
+                None
+            })
             .collect();
         if links.is_empty() {
+            // old style target elements.
+            // use child rather than descendant, because in google search engine
+            // a node's structure is like this:
+            // <div class="rc">
+            //   <a href="test_link"></a>
+            //   <span><a href="not we need"></a></span>
+            // </div>
             links = doc
                 .find(Class("r").descendant(Name("a")))
                 .filter_map(|node| node.attr("href"))
